42a8a9cf71e84ac1ed2e13fc27101b30302a24ff
Mario Godinez Medina <futureweb2000@gmail.com>
2015-11-24 17:23:40 -0600

    Basico para el proyecto

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready(function(){
+
+
+  var a = $('<a>', {
+  	href: 'http://platzi.com',
+  	target: '_blank',
+  	html: 'ir a platzi'
+  })
+
+  $('#app-body').append(a);
+
+
+})
